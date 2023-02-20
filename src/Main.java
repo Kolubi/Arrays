ccc09811493ebbd3cde3c5a9ea0eab0af6539f4a
sergei_korotaevskii <kolubisp@gmail.com>
2023-02-20 15:41:07 +0300

    20/02

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wod!");
+        int n = 10;
+        int[] array = new int[n];
+        Random rnd = new Random();
+        for (int i = 0; i < array.length; i++) {
+            int x = rnd.nextInt(10);
+            array[i] =x;
+            if(x % 2 == 0){
+                System.out.print( x + " ");
+            }
+        }
     }
 }
